fddea26f5446f8e860e5bcd38d218ea1a01a911a
aniqatc <aniqatc97@gmail.com>
2022-11-15 01:02:36 -0500

    Add Mobile Support for Form Submission (Date Input)

--- form.js
+++ form.js
@@ -2,15 +2,20 @@ let confirmVisit = () => {
 	let nameInput = document.querySelector('#name').value;
 	let visitorNumber = document.querySelector('#guests').value;
 	let availableDate = document.querySelector('#date').value;
+	let availableDateMobile = document.querySelector('#mobile-date').value;
 	let submissionMsg = document.querySelector('#submission-complete');
 	let form = document.querySelector('.tour-form');
 	let incompleteForm = document.querySelector('#submission-incomplete');
 
-	if (nameInput == false || visitorNumber == false || availableDate == false) {
+	if (
+		nameInput == false ||
+		visitorNumber == false ||
+		(availableDate == false && availableDateMobile == false)
+	) {
 		incompleteForm.textContent = `Sorry, we need more information to continue.`;
 	} else {
 		form.remove();
-		submissionMsg.textContent = `Great news, ${nameInput}! We can absolutely accomodate you and your ${visitorNumber} guest(s). Expect a call by ${availableDate}!`;
+		submissionMsg.textContent = `Great news, ${nameInput}! We can absolutely accomodate you and your ${visitorNumber} guest(s). Expect a call by ${availableDate}${availableDateMobile}!`;
 	}
 };
 
